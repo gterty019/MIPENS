3386855031bfb77a86b96e8bfbe7b44f7948c01a
gterty019 <gterty01@ucy.ac.cy>
2025-01-08 13:21:44 +0200

    Update 2_INFERENCE_ALGORITHM_CLASS.py

--- INFERENCE_SCRIPTS/2_INFERENCE_ALGORITHM_CLASS.py
+++ INFERENCE_SCRIPTS/2_INFERENCE_ALGORITHM_CLASS.py
@@ -55,7 +55,7 @@ if weights.shape != (len(models), n_classes):
     raise ValueError("Shape of weights must be (number of models, number of classes)")
 
 # Stratified K-Folds cross-validator
-skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42,test_size=0.2)
+skf = StratifiedKFold(n_splits=1, shuffle=True, random_state=42,test_size=0.2)
 
 
 
